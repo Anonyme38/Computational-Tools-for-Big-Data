bda8c8e475afdee717a5b2f700d89adb1603bf9c
Anonyme38 <32598689+Anonyme38@users.noreply.github.com>
2017-10-12 15:12:03 +0200

    Update ex1_sqlite.py

--- SQL_NO-SQL/ex1_sqlite.py
+++ SQL_NO-SQL/ex1_sqlite.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('northwind.db')
 conn.text_factory = bytes
 
-request = 'SELECT Orders.OrderID, Products.ProductName, Products.ProductID FROM Orders, Products INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID INNER JOIN [Order Details] ON [Order Details].OrderID = Orders.OrderID AND Products.ProductID = [Order Details].ProductID WHERE Customers.CustomerID = "ALFKI" ORDER BY Orders.OrderID'
+request = 'SELECT Orders.OrderID, Products.ProductName, Products.ProductID FROM Orders, Products INNER JOIN [Order Details] ON [Order Details].OrderID = Orders.OrderID AND Products.ProductID = [Order Details].ProductID WHERE Orders.CustomerID = "ALFKI" ORDER BY Orders.OrderID'
 
 i = 0
 for row in conn.execute(request):
